48c7a00d89dac536a39998e1ad0bf0ca974bf9cd
Lok Tep <you@example.com>
2015-11-11 21:02:19 +0100

    elf

--- makefiles/nuttx/firmware_nuttx.mk
+++ makefiles/nuttx/firmware_nuttx.mk
@@ -18,6 +18,7 @@ $(STAGED_FIRMWARES): $(IMAGE_DIR)%.px4: $(BUILD_DIR)%.build/firmware.px4
 	@$(ECHO) %% Copying $@
 	$(Q) $(COPY) $< $@
 	$(Q) $(COPY) $(patsubst %.px4,%.bin,$<) $(patsubst %.px4,%.bin,$@)
+	$(Q) $(COPY) $(patsubst %.px4,%.elf,$<) $(patsubst %.px4,%.elf,$@)
 
 #
 # Generate FIRMWARES.
